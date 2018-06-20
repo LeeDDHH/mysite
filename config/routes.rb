246f0818001f7e3d763d400d7dd33422195fcb1d
LeeDDHH <niche3600@gmail.com>
2018-06-20 10:23:47 +0900

    bundle update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'main/homepage'
+  root to: 'main#homepage'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

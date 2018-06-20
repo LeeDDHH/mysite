8992a6ba76cfa26ba1dba52324a5f8579edc7e0e
LeeDDHH <niche3600@gmail.com>
2018-06-20 23:56:14 +0900

    add sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'main/homepage'
+
+  resources :personal
+  
   root to: 'main#homepage'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
